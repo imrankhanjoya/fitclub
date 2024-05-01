883e7f1c9755bc4d53bcdf75b0888b0a1c1c7487
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2024-05-02 03:39:14 +0530

    all

---
+++ src/app/lib/schema/postSchema.js
@@ -0,0 +1,12 @@
+import * as z  from "zod";
+
+const postSchema = z.object({
+    description: z.string().min(20,{ message: "Enter post detail." }),
+    address: z.string().min(2,{ message: "Enter address line." }),
+    region: z.string().min(2,{ message: "Enter region." }),
+    city: z.string().min(2,{ message: "Enter city." }),
+    country: z.string().min(2,{ message: "Enter country." }),
+    mode: z.string().min(2,{ message: "Enter reqired channel to reach you." }),
+});
+
+export default postSchema
